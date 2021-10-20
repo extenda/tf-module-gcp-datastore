262469207adaf791b65e2cc60737a6c7a34f77ee
Anastasiia Kovtun <anastasiia.kovtun@extendaretail.com>
2021-10-20 18:43:42 +0300

    feat: Add Datastore resource for creating indexes

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  required_version = ">= 0.12.18"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 3.89.0"
+    }
+  }
+}
+
+provider "google" {
+  region  = var.region
+}
